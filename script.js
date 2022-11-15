394cabc54c15424e0fe5e66c93f91c16aba49550
Sho Ishida <isshi0417@gmail.com>
2022-11-15 11:30:56 -1000

    Completed function to play a single round of rock paper scissors

--- script.js
+++ script.js
@@ -1,8 +1,44 @@
 const computerOption = ["rock", "paper", "scissors"];
 const computerChoice = getComputerChoice();
+const playerChoice = "rock";
 
 function getComputerChoice() {
     return computerOption[Math.floor(Math.random() * computerOption.length)]
 }
 
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice == computerChoice) {
+        return "You draw!";
+    } else if (playerChoice == "rock") {
+        if (computerChoice == "paper") {
+            return "You lose! Paper beats rock";
+        } else {
+            return "You win! Rock beats scissors";
+        }
+    } else if (playerChoice == "paper") {
+        if (computerChoice == "scissors") {
+            return "You lose! Scissors beats rock";
+        } else {
+            return "You win! Paper beats rock";
+        }
+    } else if (playerChoice == "scissors") {
+        if (computerChoice == "rock") {
+            return "You lose! Rock beats scissors";
+        } else {
+            return "You win! Scissors beats paper";
+        }
+    }
+}
 
+console.log(computerChoice);
+console.log(playerChoice);
+
+if (playerChoice == computerChoice) {
+    console.log("You draw!");
+} else if (playerChoice == "rock") {
+    if (computerChoice == "paper") {
+        console.log("You lose! Paper beats rock"); 
+    } else {
+        console.log("You win! Rock beats scissors");
+    }
+} 
