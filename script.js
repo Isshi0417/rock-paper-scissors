8585e5e63dba6f4b60651b2b0b3bd7803c2423de
Sho Ishida <isshi0417@gmail.com>
2022-11-15 11:54:23 -1000

    Revised playround() to keep score after each outcome

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const computerOption = ["rock", "paper", "scissors"];
 const playerChoice = "rock";
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     return computerOption[Math.floor(Math.random() * computerOption.length)]
@@ -10,20 +12,26 @@ function playRound(playerChoice, computerChoice) {
         return "You draw!";
     } else if (playerChoice == "rock") {
         if (computerChoice == "paper") {
+            computerScore++;
             return "You lose! Paper beats rock";
         } else {
+            playerScore++;
             return "You win! Rock beats scissors";
         }
     } else if (playerChoice == "paper") {
         if (computerChoice == "scissors") {
+            computerScore++;
             return "You lose! Scissors beats rock";
         } else {
+            playerScore++;
             return "You win! Paper beats rock";
         }
     } else if (playerChoice == "scissors") {
         if (computerChoice == "rock") {
+            computerScore++;
             return "You lose! Rock beats scissors";
         } else {
+            playerScore++;
             return "You win! Scissors beats paper";
         }
     }
@@ -31,12 +39,11 @@ function playRound(playerChoice, computerChoice) {
 
 function game() {
     for (let i = 0; i < 5; i++) {
+        let computerChoice = getComputerChoice();
         console.log(playRound(playerChoice, computerChoice));
     }
+    console.log(playerScore);
+    console.log(computerScore);
 }
 
-for (let i = 0; i < 5; i++) {
-    computerChoice = getComputerChoice();
-    console.log(computerChoice);
-    console.log(playRound(playerChoice, computerChoice));
-}
+game();
